6912e8933c78a922313075aeb2d3649b7e145e56
Luís Vieira <luis.vieira36@gmail.com>
2021-03-29 20:13:15 +0100

    Added relatório, fixed beginning

--- srcrTP1.pl
+++ srcrTP1.pl
@@ -8,30 +8,36 @@
 % PROLOG: definicoes iniciais
 
 :- op( 900,xfy,'::' ).
-:- dynamic '-'\1.
+:- dynamic '-'/1.
 
 % ----------------------------------------------------------------------
 % Utente: Idutente,NºSegSocial, Nome, Data_Nasc, Email, Telefone, Morada,
 %           Profissão, [Doenças_Crónicas], IdCentroSaúde -> {V,F}
 
-utente(1,123123123,luis,02/02/2000,teste@gmail.com,911222333,braga,
+utente(1,123123123,luis,"02-02-2000","teste@gmail.com",911222333,braga,
 	estudante,[cego|coxo],1).
 
 % ----------------------------------------------------------------------
 % centro_saúde: Idcentro, Nome, Morada, Telefone, Email -> {V,F}
 
-centro_saude(1,bragaHospital,braga,966777888,braga@hospital.com).
+centro_saude(1,bragaHospital,braga,966777888,"braga@hospital.com").
 
 % ----------------------------------------------------------------------
 % staff: Idstaff, Idcentro, Nome, email -> {V,F}
 
-staff(1,1,enfermeiro1,enfermeiro1@hospital.com).
+staff(1,1,enfermeiro1,"enfermeiro1@hospital.com").
 
 % ----------------------------------------------------------------------
 % vacinação_Covid: #Staf, #utente, Data, Vacina, Toma -> {V,F}
 
 
-vacinacao_covid(1,1,23/03/2021,pfizer,1).
+vacinacao_covid(1,1,23-03-2021,pfizer,1).
+
+
+% ----------------------------------------------------------------------
+% falta_segunda_vacina: Lista com IDs utentes a quem falta 2a vacina
+
+falta_segunda_vacina(R).
 
 % ----------------------------------------------------------------------
 % Extensao do meta-predicado nao: Questao -> {V,F}
