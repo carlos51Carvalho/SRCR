a7cd573a2e449faf54f6c4da98e7d8d3baf8f968
Luís Vieira <luis.vieira36@gmail.com>
2021-04-01 19:28:31 +0100

    added invariantes de remoção

--- srcrTP1.pl
+++ srcrTP1.pl
@@ -1,10 +1,13 @@
+% ----------------------------------------------------------------------
 % PROLOG: Declaracoes iniciais
+% ----------------------------------------------------------------------
 
 :- style_check(-discontiguous).
 :- style_check(-singleton).
 
 % ----------------------------------------------------------------------
 % PROLOG: definicoes iniciais
+% ----------------------------------------------------------------------
 
 :- op( 900,xfy,'::' ).
 :- dynamic '-'/1.
@@ -16,6 +19,7 @@
 % ----------------------------------------------------------------------
 % Utente: Idutente,NºSegSocial, Nome, Data_Nasc, Email, Telefone, Morada,
 %           Profissão, [Doenças_Crónicas], IdCentroSaúde -> {V,F}
+% ----------------------------------------------------------------------
 
 utente(1,123123123,"luis","02-02-2000","teste@gmail.com",911222333,"braga",
 	"estudante",[cego|coxo],1).
@@ -26,6 +30,7 @@ removeUtente(ID):- involucao(utente(ID,_,_,_,_,_,_,_,_,_)).
 
 % ----------------------------------------------------------------------
 % centro_saúde: Idcentro, Nome, Morada, Telefone, Email -> {V,F}
+% ----------------------------------------------------------------------
 
 centro_saude(1,bragaHospital,braga,966777888,"braga@hospital.com").
 
@@ -35,6 +40,7 @@ removeCentroSaude(ID):- involucao(centro_saude(ID,_,_,_,_)).
 
 % ----------------------------------------------------------------------
 % staff: Idstaff, Idcentro, Nome, email -> {V,F}
+% ----------------------------------------------------------------------
 
 staff(1,1,enfermeiro1,"enfermeiro1@hospital.com").
 
@@ -44,6 +50,7 @@ removeStaff(ID):- involucao(staff(ID,_,_,_)).
 
 % ----------------------------------------------------------------------
 % vacinação_Covid: #Staff, #utente, Data, Vacina, Toma -> {V,F}
+% ----------------------------------------------------------------------
 
 
 vacinacao_covid(1,1,23-03-2021,pfizer,1).
@@ -54,13 +61,15 @@ removeVacinacaoCovid(IDS,IDU):- involucao(vacinacao_covid(IDS,IDU,_,_,_)).
 
 % ----------------------------------------------------------------------
 % falta_segunda_vacina: Lista com IDs utentes a quem falta 2a vacina
+% ----------------------------------------------------------------------
 
 falta_segunda_vacina(R):-solucoes( ( _,IDU,_,V,1 ),
 		  (vacinacao_covid( IDS,IDU,D,V,1)), R ),R.
 
 
 % ----------------------------------------------------------------------
-% Invariantes
+% Invariantes de inserção
+% ----------------------------------------------------------------------
 
 
 +utente( ID,NSS,N,DN,E,T,M,P,LDC,IDCS ) :: (solucoes( (ID,NSS,N,DN,E,T,M,P,LDC,IDCS),
@@ -94,13 +103,50 @@ falta_segunda_vacina(R):-solucoes( ( _,IDU,_,V,1 ),
                   ).
 
 % ----------------------------------------------------------------------
+% Invariantes de remoção
+% ----------------------------------------------------------------------
+
+-utente( ID,NSS,N,DN,E,T,M,P,LDC,IDCS ) :: (solucoes( ID,
+		  (utente( ID,_,_,_,_,_,_,_,_,_ )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).
+
+-staff( IDS,IDC,N,E ) :: (solucoes( IDS,
+		  (staff( IDS,_,_,_ )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).
+
+-centro_saude( IDC,N,M,T,E ) :: (solucoes( IDC,
+		  (centro_saude( IDC,_,_,_,_ )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).
+
+-vacinacao_covid( IDS,IDU,D,V,1 ) :: (solucoes( (IDS,IDU,D,V,1),
+		  (vacinacao_covid( IDS,IDU,D,V,1 )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).
+
+-vacinacao_covid( IDS,IDU,D,V,2 ) :: (solucoes( (IDS,IDU,D,V,2),
+		  (vacinacao_covid( IDS,IDU,D,V,2 )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).
+
+% ----------------------------------------------------------------------
 % Extensao do meta-predicado nao: Questao -> {V,F}
+% ----------------------------------------------------------------------
 
 nao( Questao ) :-
     Questao, !, fail.
 nao( Questao ).
 
+% ----------------------------------------------------------------------
 % Extensao do predicado que permite a evolução do conhecimento
+% ----------------------------------------------------------------------
 
 evolucao( Termo ) :-
     solucoes( Invariante,+Termo::Invariante,Lista ),
@@ -118,7 +164,9 @@ solucoes( X,Y,Z ) :-
 comprimento( S,N ) :-
     length( S,N ).
 
+% ----------------------------------------------------------------------
 % Extensão do predicado que permite a involucao do conhecimento
+% ----------------------------------------------------------------------
 
 involucao(Termo):- solucoes(Invariante,-Termo::Invariante,Lista),
 		   remocao(Termo),
@@ -132,8 +180,10 @@ teste( [R|LR] ) :-
     R,
     teste( LR ).
 
+% ----------------------------------------------------------------------
 % Extensao do meta-predicado demo: Questao,Resposta -> {V,F}
 %                            Resposta = { verdadeiro,falso,desconhecido }
+% ----------------------------------------------------------------------
 
 demo( Questao,verdadeiro ) :-
     Questao.
