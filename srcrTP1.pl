042e6ea9db9244777fe3d5289c42cd09ef2af7b8
Luís Vieira <luis.vieira36@gmail.com>
2021-04-01 15:34:36 +0100

    Added Invariantes

--- srcrTP1.pl
+++ srcrTP1.pl
@@ -1,21 +1,23 @@
 % PROLOG: Declaracoes iniciais
 
-:- set_prolog_flag( discontiguous_warnings,off ).
-:- set_prolog_flag( single_var_warnings,off ).
-:- set_prolog_flag( unknown,fail ).
+
 
 % ----------------------------------------------------------------------
 % PROLOG: definicoes iniciais
 
 :- op( 900,xfy,'::' ).
 :- dynamic '-'/1.
+:- dynamic utente/10.
+:- dynamic centro_saude/5.
+:- dynamic staff/4.
+:- dynamic vacinacao_covid/5.
 
 % ----------------------------------------------------------------------
 % Utente: Idutente,NºSegSocial, Nome, Data_Nasc, Email, Telefone, Morada,
 %           Profissão, [Doenças_Crónicas], IdCentroSaúde -> {V,F}
 
-utente(1,123123123,luis,"02-02-2000","teste@gmail.com",911222333,braga,
-	estudante,[cego|coxo],1).
+utente(1,123123123,"luis","02-02-2000","teste@gmail.com",911222333,"braga",
+	"estudante",[cego|coxo],1).
 
 % ----------------------------------------------------------------------
 % centro_saúde: Idcentro, Nome, Morada, Telefone, Email -> {V,F}
@@ -28,7 +30,7 @@ centro_saude(1,bragaHospital,braga,966777888,"braga@hospital.com").
 staff(1,1,enfermeiro1,"enfermeiro1@hospital.com").
 
 % ----------------------------------------------------------------------
-% vacinação_Covid: #Staf, #utente, Data, Vacina, Toma -> {V,F}
+% vacinação_Covid: #Staff, #utente, Data, Vacina, Toma -> {V,F}
 
 
 vacinacao_covid(1,1,23-03-2021,pfizer,1).
@@ -38,6 +40,41 @@ vacinacao_covid(1,1,23-03-2021,pfizer,1).
 % falta_segunda_vacina: Lista com IDs utentes a quem falta 2a vacina
 
 falta_segunda_vacina(R).
+
+
+% ----------------------------------------------------------------------
+% Invariantes
+
+
++utente( ID,NSS,N,DN,E,T,M,P,LDC,IDCS ) :: (solucoes( (ID,NSS,N,DN,E,T,M,P,LDC,IDCS),
+		  (utente( ID,NSS,N,DN,E,T,M,P,LDC,IDCS )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).
+
++staff( IDS,IDC,N,E ) :: (solucoes( (IDS,IDC,N,E),
+		  (staff( IDS,IDC,N,E )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).
+
++centro_saude( IDC,N,M,T,E ) :: (solucoes( (IDC,N,M,T,E),
+		  (centro_saude( IDC,N,M,T,E )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).
+
++vacinacao_covid( IDS,IDU,D,V,1 ) :: (solucoes( (IDS,IDU,D,V,1),
+		  (vacinacao_covid( IDS,IDU,D,V,1 )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).
+
++vacinacao_covid( IDS,IDU,D,V,2 ) :: (solucoes( (IDS,IDU,D,V,2),
+		  (vacinacao_covid( IDS,IDU,D,V,2 )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).
 
 % ----------------------------------------------------------------------
 % Extensao do meta-predicado nao: Questao -> {V,F}
