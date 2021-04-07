bb1c061e1f3463ddece2755386446f46839da306
Luís Vieira <luis.vieira36@gmail.com>
2021-04-07 21:20:16 +0100

    povoamento, update em invariantes,, fixed some errors.

--- srcrTP1.pl
+++ srcrTP1.pl
@@ -94,65 +94,110 @@ morethan65_notvacinated(RS):- solucoes((ID,N),(utente(ID,_,N,D-M-A,_,_,_,_,_,_),
 %           Profissão, [Doenças_Crónicas], IdCentroSaúde -> {V,F}
 % ----------------------------------------------------------------------
 
-utente(1,123123123,"luis",02-02-2000,"teste@gmail.com",911222333,"braga",
-	"estudante",[cego|coxo],1).
+% Vacinado indevidamente
 
-utente(2,123123124,"joao",02-02-2000,"teste2@gmail.com",911222334,"braga",
-	"estudante",[Insuficiencia_renal],1).
+utente(1,123123123,"Luis",02-02-2000,"luis@gmail.com",911222333,"braga","estudante",[cego,coxo],1).
+utente(5,123456789,"Marta",02-02-1993,"marta@gmail.com",912345677,"algarve","esteticista",[],3).
 
-utente(3,124123124,"manuel",02-02-1969,"teste3@gmail.com",911232334,"braga",
-	"velho",[Insuficiencia_cardiaca, Insuficiencia_renal, Insuficiencia_hepatica],1).
+% Não pertence a nenhuma das fases
+utente(2,123123124,"Joao",24-04-2000,"joao@gmail.com",911222334,"braga","estudante",[Insuficiencia_renal],2).
 
-utente(4,124123125,"xavier",02-02-1930,"testePri@gmail.com",911232355,"alentejo",
-	"enfermeiro",[],1).
+% Pertence a ambas as fases
+utente(3,124123124,"Manuel",02-02-1969,"manuel@gmail.com",911232334,"braga","arquiteto",[Insuficiencia_cardiaca, Insuficiencia_renal, Insuficiencia_hepatica],1).
+
+% Pertence a ambas as fases - candidato
+utente(4,124123125,"Xavier",02-02-1930,"xavier@gmail.com",911232355,"alentejo","enfermeiro",[],3).
+
+% Pertence a 2a fase - candidato
+utente(6,124123129,"Oliver",02-02-1958,"oliver@gmail.com",911232634,"braga","professor",[Insuficiencia_hepatica],2).
+
+% ----------------------------------------------------------------------
+% Extensão do predicado showAllUtentes 
+%                       que apresenta ao utilizador todos os utentes
+% ----------------------------------------------------------------------
 
 showAllUtentes():- listing(utente).
 
 registaUtente(ID,NSS,N,DT,E,T,M,P,LDC,IDCS):- evolucao(utente(ID,NSS,N,DT,E,T,M,P,LDC,IDCS)).
 
-removeUtente(ID):- involucao(utente(ID,_,_,_,_,_,_,_,_,_)).
+removeUtenteID(ID):- involucao(utente(ID,_,_,_,_,_,_,_,_,_)).
 
-removeUtente(NSS):- involucao(utente(_,NSS,_,_,_,_,_,_,_,_)).
+removeUtenteNSS(NSS):- involucao(utente(_,NSS,_,_,_,_,_,_,_,_)).
+
+removeUtenteAllInfoByID(ID):- solucoes(ID,(vacinacao_covid(_,ID,_,_,_)),R), removeInfoVacinacao(R), removeUtenteID(ID).
+
+removeInfoVacinacao([]).
+removeInfoVacinacao([IDU|Tail]):- removeVacinacaoCovid(IDU),removeInfoVacinacao(Tail).
 
 % ----------------------------------------------------------------------
 % centro_saúde: Idcentro, Nome, Morada, Telefone, Email -> {V,F}
 % ----------------------------------------------------------------------
 
-centro_saude(1,bragaHospital,braga,966777888,"braga@hospital.com").
+centro_saude(1,"Hospital de Braga","Braga",253027000,"hospital_braga@sns.pt").
+centro_saude(2,"Hospital da Trofa Braga Norte","Braga",253027002,"hospital_trofa_braga@sns.pt").
+centro_saude(3,"Hospital de São João","Porto",225512100,"hospital_sao_joao@sns.pt").
+
+% ---------------------------------------------------------------------------
+% Extensão do predicado showAllCentroSaude 
+%                       que apresenta ao utilizador todos os centros de saude
+% ---------------------------------------------------------------------------
 
 showAllCentroSaude():- listing(centro_saude).
 
 registaCentroSaude(ID,N,M,T,E):- evolucao(centro_saude(ID,N,M,T,E)).
 
-removeCentroSaude(ID):- involucao(centro_saude(ID,_,_,_,_)).
+removeCentroSaudeID(ID):- involucao(centro_saude(ID,_,_,_,_)).
 
 % ----------------------------------------------------------------------
 % staff: Idstaff, Idcentro, Nome, email -> {V,F}
 % ----------------------------------------------------------------------
 
-staff(1,1,enfermeiro1,"enfermeiro1@hospital.com").
+staff(1,1,"Jorge","jorge_andrade@gmail.com").
+staff(2,2,"Liliana","liliana_albernaz@gmail.com").
+staff(3,3,"Andre","martinsdr@gmail.com").
+
+% ----------------------------------------------------------------------
+% Extensão do predicado showAllStaff 
+%                       que apresenta ao utilizador todo o staff
+% ----------------------------------------------------------------------
 
 showAllStaff():- listing(staff).
 
 registaStaff(IDS,IDC,N,E):- evolucao(staff(IDS,IDC,N,E)).
 
-removeStaff(ID):- involucao(staff(ID,_,_,_)).
+removeStaffID(ID):- involucao(staff(ID,_,_,_)).
 
 % ----------------------------------------------------------------------
 % vacinação_Covid: #Staff, #utente, Data, Vacina, Toma -> {V,F}
 %                   pfizer, astrazeneca
 % ----------------------------------------------------------------------
 
+% Vacinacao invalida - nao pertence a nenhuma das fase
 
 vacinacao_covid(1,1,23-03-2021,pfizer,1).
 vacinacao_covid(1,1,31-03-2021,pfizer,2).
-vacinacao_covid(1,2,07-04-2021,pfizer,1).
+
+vacinacao_covid(3,5,23-03-2021,pfizer,1).
+
+vacinacao_covid(2,3,07-04-2021,astrezeneca,1).
+
+% Vacinacao valida - pertence a ambas as fases
+
+vacinacao_covid(3,2,07-04-2021,pfizer,1).
+
+% Vacinacao valida - pertence a segunda fase
+
+
+% ----------------------------------------------------------------------
+% Extensão do predicado showAllVacinacao 
+%                       que apresenta ao utilizador todas as vacinacoes
+% ----------------------------------------------------------------------
 
 showAllVacinacao():- listing(vacinacao_covid).
 
 registaVacinacaoCovid(IDS,IDU,D,V,T):- evolucao(vacinacao_covid(IDS,IDU,D,V,T)).
 
-removeVacinacaoCovid(IDS,IDU):- involucao(vacinacao_covid(IDS,IDU,_,_,_)).
+removeVacinacaoCovid(IDU):- involucao(vacinacao_covid(_,IDU,_,_,_)).
 
 % ----------------------------------------------------------------------
 % falta_segunda_vacina: Lista com IDs e nomes de utentes a quem falta 2a vacina
@@ -185,15 +230,17 @@ vacinado_indevidamente(RS) :- solucoes((ID,N),(utente(ID,_,N,_,_,_,_,_,_,_),
 
 vacinado_indevidamente_1afase(RS):- solucoes((ID,N),
                                     (utente(ID,_,N,_,_,_,_,_,_,_),
-                                    vacinado(X),pertence((ID,N),X),
+                                    atleast_one_vacina(ID),
                                     nao(check_1afase((ID,N)))),R),
                                     remove_dups(R,RS).
 
 vacinado_indevidamente_2afase(RS):- solucoes((ID,N),
                                     (utente(ID,_,N,_,_,_,_,_,_,_),
-                                    vacinado(X),pertence((ID,N),X),
+                                    atleast_one_vacina(ID),
                                     nao(check_2afase((ID,N)))),R),
                                     remove_dups(R,RS).
+
+atleast_one_vacina(ID):- solucoes((ID,N),(utente(ID,_,N,_,_,_,_,_,_,_),vacinacao_covid(_,ID,_,_,_)),R),comprimento(R,N), N>=1.
 
 check_1afase((ID,N)):- primeira_fase(Y),pertence((ID,N),Y).
 
@@ -214,70 +261,148 @@ candidato(RS):- primeira_fase(X),segunda_fase(Y),concatenar(X,Y,RS).
 % Invariantes de inserção
 % ----------------------------------------------------------------------
 
+% ----------------------------------------------------------------------
+% Utente
+% ----------------------------------------------------------------------
 
-+utente( ID,NSS,N,DN,E,T,M,P,LDC,IDCS ) :: (solucoes( ID,
+% Verificar se já existe utente com este ID
++utente( ID,_,_,_,_,_,_,_,_,_ ) :: (solucoes( ID,
 		  (utente( ID,_,_,_,_,_,_,_,_,_ )), S ),
                   comprimento( S,NS ), 
 		  NS == 1
                   ).
 
-+staff( IDS,IDC,N,E ) :: (solucoes( IDS,
-		  (staff( IDS,_,_,_ )), S ),
+% Verificar se já existe utente com este número de segurança social
++utente( _,NSS,_,_,_,_,_,_,_,_ ) :: (solucoes( NSS,
+		  (utente( _,NSS,_,_,_,_,_,_,_,_ )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).   
+
+% Verificar se existe email
++utente( _,_,_,_,E,_,_,_,_,_ ) :: (solucoes( E,
+		  (utente( _,_,_,_,E,_,_,_,_,_ )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).  
+
+% Verificar se existe telefone
++utente( _,_,_,_,_,T,_,_,_,_ ) :: (solucoes( T,
+		  (utente( _,_,_,_,_,T,_,_,_,_ )), S ),
                   comprimento( S,NS ), 
 		  NS == 1
                   ).
 
-+centro_saude( IDC,N,M,T,E ) :: (solucoes( IDC,
+% Verificar se existe centro de saúde
++utente( _,_,_,_,_,_,_,_,_,IDCS ) :: (solucoes( IDCS,
+		  (centro_saude( IDCS,_,_,_,_ )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).
+
+
+
+% ----------------------------------------------------------------------
+% Staff
+% ----------------------------------------------------------------------
+
+
+% Verificar se já existe staff inserido com este ID
++staff( IDS,_,_,_ ) :: (solucoes( IDS,
+		  (staff( IDS,_,_,_ )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).
+                  
+% Verificar se existe centro_saude inserido                  
++staff( _,IDCS,_,_ ) :: (solucoes( IDCS,
+		  (centro_saude( IDCS,_,_,_,_ )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).
+
+% Verificar se existe email
++staff( _,_,_,E ) :: (solucoes( E,
+		  (staff( _,_,_,E )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).                  
+
+% ----------------------------------------------------------------------
+% Centro de Saude
+% ----------------------------------------------------------------------
+
+
+% Verificar se já existe centro_saude inserido com este ID
++centro_saude( IDC,_,_,_,_ ) :: (solucoes( IDC,
 		  (centro_saude( IDC,_,_,_,_ )), S ),
                   comprimento( S,NS ), 
 		  NS == 1
                   ).
 
-+vacinacao_covid( IDS,IDU,D,V,1 ) :: (solucoes( IDU,
-		  (vacinacao_covid( _,IDU,_,_,1 )), S ),
+% Verificar se existe telefone 
++centro_saude( _,_,_,T,_ ) :: (solucoes( T,
+		  (centro_saude( _,_,_,T,_ )), S ),
                   comprimento( S,NS ), 
 		  NS == 1
                   ).
 
-+vacinacao_covid( IDS,IDU,D,V,2 ) :: (solucoes( IDU,
-		  (vacinacao_covid( _,IDU,_,_,2 )), S ),
+% Verificar se existe email
++centro_saude( _,_,_,_,E ) :: (solucoes( E,
+		  (centro_saude( _,_,_,_,E )), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).                  
+
+% ----------------------------------------------------------------------
+% Vacinação Covid
+% ----------------------------------------------------------------------
+
+% Verificar se existe staff inserido
++vacinacao_covid( IDS,_,_,_,_ ) :: (solucoes( IDS,
+		  (staff(IDS,_,_,_)), S ),
                   comprimento( S,NS ), 
 		  NS == 1
                   ).
+
+% Verificar se existe utente inserido
++vacinacao_covid( _,IDU,_,_,_ ) :: (solucoes( IDU,
+		  (utente(IDU,_,_,_,_,_,_,_,_,_)), S ),
+                  comprimento( S,NS ), 
+		  NS == 1
+                  ).
+
+
 
 % ----------------------------------------------------------------------
 % Invariantes de remoção
 % ----------------------------------------------------------------------
 
--utente( ID,NSS,N,DN,E,T,M,P,LDC,IDCS ) :: (solucoes( ID,
-		  (utente( ID,_,_,_,_,_,_,_,_,_ )), S ),
+-utente( ID,NSS,N,DN,E,T,M,P,LDC,IDCS ) :: (solucoes( (ID,NSS,N,DN,E,T,M,P,LDC,IDCS),
+		  (utente( ID,NSS,N,DN,E,T,M,P,LDC,IDCS )), S ),
                   comprimento( S,NS ), 
 		  NS == 1
                   ).
 
--staff( IDS,IDC,N,E ) :: (solucoes( IDS,
-		  (staff( IDS,_,_,_ )), S ),
+-staff( IDS,IDC,N,E ) :: (solucoes( (IDS,IDC,N,E),
+		  (staff( IDS,IDC,N,E )), S ),
                   comprimento( S,NS ), 
 		  NS == 1
                   ).
 
--centro_saude( IDC,N,M,T,E ) :: (solucoes( IDC,
-		  (centro_saude( IDC,_,_,_,_ )), S ),
+-centro_saude( IDC,N,M,T,E ) :: (solucoes( (IDC,N,M,T,E),
+		  (centro_saude( IDC,N,M,T,E )), S ),
                   comprimento( S,NS ), 
 		  NS == 1
                   ).
 
--vacinacao_covid( IDS,IDU,D,V,1 ) :: (solucoes( IDU,
-		  (vacinacao_covid( _,IDU,_,_,1 )), S ),
+-vacinacao_covid( IDS,IDU,D,V,T ) :: (solucoes( (IDS,IDU,D,V,T),
+		  (vacinacao_covid( IDS,IDU,D,V,T )), S ),
                   comprimento( S,NS ), 
-		  NS == 1
+		  NS =< 2
                   ).
 
--vacinacao_covid( IDS,IDU,D,V,2 ) :: (solucoes( IDU,
-		  (vacinacao_covid( _,IDU,_,_,2 )), S ),
-                  comprimento( S,NS ), 
-		  NS == 1
-                  ).
+
 
 % ----------------------------------------------------------------------
 % Extensao do meta-predicado nao: Questao -> {V,F}
@@ -312,8 +437,8 @@ comprimento( S,N ) :-
 % ----------------------------------------------------------------------
 
 involucao(Termo):- solucoes(Invariante,-Termo::Invariante,Lista),
-		   remocao(Termo),
-                   teste(Lista).
+                   teste(Lista),
+		           remocao(Termo).
 
 remocao(Termo):- retract(Termo).
 remocao(Termo):- assert(Termo),!,fail.
@@ -358,5 +483,9 @@ concatenar(T,[],T).
 concatenar([],T,T).
 concatenar([H|T],L,F):- adicionar(H,N,F), concatenar(T,L,N).
 
+
+% ----------------------------------------------------------------------
+% Extensao do predicado intersecção: Lista1, Lista2, ListaFinal 
+% ----------------------------------------------------------------------
 
 intersection(A,B,AnB):- subtract(A,B,AminusB),subtract(A,AminusB,K),sort(K,AnB).
